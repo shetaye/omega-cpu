b861735a0a42c7a7418de75f3b8267e6bd2f0311
Hyperion302 <Hyperion302@gmail.com>
2018-04-28 15:02:09 -0500

    Wrote up nextToken

--- Demos/Calculator/mathScanner.s
+++ Demos/Calculator/mathScanner.s
@@ -31,8 +31,8 @@ nextToken_case0:
 nextToken_case1:
 	LA $r8,buffer
 	LW $r9,$r8
-	EQI $r9,48
-	BZ nextToken_else1
+	EQI $r10,$r9,48
+	BZI $r10,nextToken_else1
 #; state = 3;
 	LA $r10,state
 	ADDI $r11,$r0,3
@@ -49,12 +49,10 @@ nextToken_case1:
 #; break;
 	J nextToken_switchFinished
 nextToken_else1:
-	ADD $r10,$r0,$r9
-	ADD $r11,$r0,$r9
-	LTI $r10,49
-	GTI $r11,58
+	LTI $r10,$r9,49
+	GTI $r11,$r9,58
 	OR $r10,$r10,$r11
-	BZ nextToken_else2
+	BZI $r10,nextToken_else2
 #; state = 2;
 	LA $r10,state
 	LW $r11,$r10
@@ -78,19 +76,18 @@ nextToken_else1:
 #; break;
 	J nextToken_switchFinished
 nextToken_else2:
-	ADD $r10,$r0,$r9
-	EQI $r10,43
-	EQI $r11,45
+	EQI $r10,$r9,43
+	EQI $r11,$r9,45
 	OR $r10,$r10,$r11
-	EQI $r11,42
+	EQI $r11,$r9,42
 	OR $r10,$r10,$r11
-	EQI $r11,47
+	EQI $r11,$r9,47
 	OR $r10,$r10,$r11
-	EQI $r11,40
+	EQI $r11,$r9,40
 	OR $r10,$r10,$r11
-	EQI $r11,41
+	EQI $r11,$r9,41
 	OR $r10,$r10,$r11
-	BZ nextToken_else3
+	BZ $r10,nextToken_else3
 #; state = 4;
 	LA $r10,state
 	ADDI $r11,$r0,4
@@ -107,11 +104,10 @@ nextToken_else2:
 #; break;
 	J nextToken_switchFinished
 nextToken_else3:
-	ADD $r10,$r0,$r9
-	EQI $r10,32
-	EQI $r11,9
+	EQI $r10,$r9,32
+	EQI $r11,$r9,9
 	OR $r10,$r10,$r11
-	BZ nextToken_else4
+	BZI $r10,nextToken_else4
 #; state = 1;
 	LA $r10,state
 	ADDI $r11,$r0,1
@@ -150,12 +146,10 @@ nextToken_else5:
 nextToken_case2:
 	LA $r9,buffer
 	LW $r9,$r9
-	ADD $r10,$r0,$r9
-	ADD $r11,$r0,$r9
-	LTI $r10,48
-	GTI $r11,57
+	LTI $r10,$r9,48
+	GTI $r11,$r9,57
 	OR $r10,$r10,$r11
-	BNZ nextToken_else6
+	BNZ $r10,nextToken_else6
 #; number = number * 10 + (buffer - '0');
 #; number * 10
 	LA $r10,number
@@ -170,18 +164,117 @@ nextToken_case2:
 	LA $r11,number
 	LW $r12,$r11
 	ADD $r10,$r10,$r12
-	SW $r11,$r10	
-nextToken_else6:	
+	SW $r11,$r10
+#; prevcharacter = buffer;
+	LA $r10,buffer
+	LW $r10,$r10
+	LA $r11,prevcharacter
+	SW $r11,$r10
+#; buffer = getchar();
+	LA $r10,buffer
+	INPB $r11,$p1
+	SW $r10,$r11
+#; break;
+	J nextToken_switchFinished
+nextToken_else6:
+#; p[0] = TOKEN_NUMBER;
+	ADDI $r10,$r0,0
+	SW $r4,$r10
+#; p[1] = number;
+	LA $r10,number
+	LW $r10,$r10
+	ADDI $r11,$r4,4
+	SW $r11,$r10
+#; return;
+	ret
+#; break; (Why? we just returned!)
 	J nextToken_switchFinished
 nextToken_case3:
+#; state = 1;
+	LA $r10,state
+	ADDI $r11,$r0,1
+	SW $r10,$r11
+#; p[0] = TOKEN_NUMBER;
+	ADDI $r10,$r0,0
+	SW $r4,$r10
+#; p[1] = 0;
+	SW $r4,$r0
+#; return;
+	ret
+#; break;
 	J nextToken_switchFinished
 nextToken_case4:
+#; state = 1;
+	LA $r10,state
+	ADDI $r11,$r0,1
+	SW $r10,$r11
+#; switch statement turned to if statement:
+	LA $r10,prevcharacter
+	LW $r10,$r10
+	EQI $r10,$r10,43
+	BZI $r10,nextToken_else7
+#; p[0] = TOKEN_PLUS;
+	ADDI $r10,$r0,1
+	SW $r4,$r10
+#; break;
+	J nextToken_switch2Finished
+nextToken_else7:
+	LA $r10,prevcharacter
+	LW $r10,$r10
+	EQI $r10,$r10,45
+	BZI $r10,nextToken_else8
+#; p[0] = TOKEN_MINUS;
+	ADDI $r10,$r0,2
+	SW $r4,$r10
+#; break;
+	J nextToken_switch2Finished
+nextToken_else8:
+	LA $r10,prevcharacter
+	LW $r10,$r10
+	EQI $r10,$r10,47
+	BZI $r10,nextToken_else9
+#; p[0] = TOKEN_DIV;
+	ADDI $r10,$r0,4
+	SW $r4,$r10
+#; break;
+	J nextToken_switch2Finished
+nextToken_else9:
+	LA $r10,prevcharacter
+	LW $r10,$r10
+	EQI $r10,$r10,42
+	BZI $r10,nextToken_else10
+#; p[0] = TOKEN_MULT;
+	ADDI $r10,$r0,3
+	SW $r4,$r10
+#; break;
+	J nextToken_switch2Finished
+nextToken_else10:
+	LA $r10,prevcharacter
+	LW $r10,$r10
+	EQI $r10,$r10,40
+	BZI $r10,nextToken_else11
+#; p[0] = TOKEN_LEFTP;
+	ADDI $r10,$r0,5
+	SW $r4,$r10
+#; break;
+	J nextToken_switch2Finished
+nextToken_else11:
+#; p[0] = TOKEN_RIGHTP;
+	ADDI $r10,$r0,6
+	SW $r4,$r10
+nextToken_switch2Finished:
+#; p[1] = 0;
+	ADDI $r10,$r4,4
+	SW $r10,$r0
+#; return;
+	ret
+#; break;
 	J nextToken_switchFinished
 nextToken_switchFinished:
 	LA $r8,state
 	LW $r8,$r8
-	SUBI $r8,$r8,1
-	BNZI $r8,nextToken_doWhile_stateNot1
+	EQI $r8,$r8,1
+	BZI $r8,nextToken_doWhile_stateNot1
 #; TODO: Insert reset_stack
 #; TODO: Insert push
 #; TODO: Insert pop
